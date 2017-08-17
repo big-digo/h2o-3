a37b6d9ca09567c9035da30a8a52f0a66776446f
Michal Raška <michal.raska@gmail.com>
2017-08-17 11:06:17 +0200

    Moving to docker agent

--- h2o-hadoop/scripts/docker/cdh/scripts/startup.sh
+++ h2o-hadoop/scripts/docker/cdh/scripts/startup.sh
@@ -28,31 +28,40 @@ if [[ $(echo ${RUN_TESTS} | tr -s '[:upper:]' '[:lower:]') == 'true' ]]; then
   failed=$((0))
   succeeded=$((0))
   total=$((0))
-  cd /home/h2o/tests/python
-  echo
-  echo "Running tests from $(pwd)"
-  for x in $(ls *.py); do
-    total=$(($total + 1))
+  ls /home/h2o/
+  ls /home/h2o/tests
+  ls /home/h2o/tests/python
+  if [[ -d /home/h2o/tests/python ]]; then
+    cd /home/h2o/tests/python
     echo
-    echo "####### Running test file ${x} #######"
-    unset e
-    python ${x}
-    set -e
-    if [ $? -ne 0 ]; then
-      failed=$(($failed + 1))
-      echo "###### FAILED ######"
-      retval=1
-    else
-      succeeded=$(($succeeded + 1))
-    fi
-    echo "###### Test file ${x} completed ######"
-    echo
-  done
-  echo "###### TEST RESULTS ######"
-  echo -e "\tSUCCEEDED:\t${succeeded}"
-  echo -e "\tFAILED:\t\t${failed}"
-  echo "--------------------------"
-  echo -e "\tTOTAL:\t\t${total}"
+    echo "Running tests from $(pwd)"
+    for x in $(ls *.py); do
+      total=$(($total + 1))
+      echo
+      echo "####### Running test file ${x} #######"
+      unset e
+      sudo -u h2o python ${x}
+      set -e
+      if [ $? -ne 0 ]; then
+        failed=$(($failed + 1))
+        echo "###### FAILED ######"
+        retval=1
+      else
+        succeeded=$(($succeeded + 1))
+      fi
+      echo "###### Test file ${x} completed ######"
+      echo
+    done
+    echo "###### TEST RESULTS ######"
+    echo -e "\tSUCCEEDED:\t${succeeded}"
+    echo -e "\tFAILED:\t\t${failed}"
+    echo "--------------------------"
+    echo -e "\tTOTAL:\t\t${total}"
+  else
+    echo "No Python tests found!"
+  fi
+else
+  echo "Skipping tests"
 fi
 
 if [[ $(echo ${ENTER_BASH} | tr -s '[:upper:]' '[:lower:]') == 'true' ]]; then
